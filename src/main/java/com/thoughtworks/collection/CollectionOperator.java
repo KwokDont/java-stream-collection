b63e322047949d2e5fda961c6fec860a07e5818e
unknown <11059964896@qq.com>
2019-07-14 15:18:34 +0800

    finish and test collection practice

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,23 +2,43 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        int min = Math.min(left, right);
+        int max = Math.max(left, right);
+        return IntStream.rangeClosed(min, max)
+                .boxed()
+                .filter(num -> num >= min && num <= max)
+                .sorted((i, j) -> left < right ? i - j : j - i)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        int min = Math.min(left, right);
+        int max = Math.max(left, right);
+        return IntStream.rangeClosed(min, max)
+                .boxed()
+                .filter(num -> num >= min && num <= max && num % 2 == 0)
+                .sorted((i, j) -> left < right ? i - j : j - i)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        return Arrays.stream(array)
+                .filter(number -> number % 2 == 0)
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+        return Arrays.stream(array)
+                .reduce((now,then) -> then)
+                .getAsInt();
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
